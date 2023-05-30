0b0291b2735a80a298850b3718cb222050ed127b
chnnxyz <s@chnn.xyz>
2023-05-30 14:20:00 -0600

    logical or

--- elixir/tsfuzz/lib/fuzzy/logic.ex
+++ elixir/tsfuzz/lib/fuzzy/logic.ex
@@ -58,4 +58,38 @@ defmodule Tsfuzz.Fuzzy.Logic.Or do
   """
   def or_max(a, b), do: max(a, b)
 
+  def or_probsum(a, b), do: a + b - (a * b)
+
+  def or_lukasiewicz(a, b), do: min(a + b, 1)
+
+  def or_drastic(a, b) do
+    t = fn _ ->
+      cond do
+        a == 0 ->
+          0
+        b == 0 ->
+          0
+        true ->
+          1
+      end
+    end
+
+    t
+  end
+
+  def or_nilpotent(a, b) do
+    t = fn _ ->
+      cond do
+        a + b < 1 ->
+          max(a, b)
+        true ->
+          1
+      end
+    end
+
+    t
+  end
+
+  def or_hamacher(a, b), do: (a + b) / (1 + a * b)
+
 end
